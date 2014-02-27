4c9a6e847b03a056129913c32290deb353c33cbf
Andy Rominger <ajrominger@gmail.com>
2014-02-26 16:52:57 -0800

    updated version of logLik z-score function

--- R/logLik_meteDist.R
+++ R/logLik_meteDist.R
@@ -1,42 +1,21 @@
-#' @title Title of function
-#'
-#' @description
-#' \code{function.name} what it does
-#'
-#' @details
-#' how it works
-#' etc.
-#' 
-#' @param arg description of arg
-#' @param arg description of arg
-#' @keywords manip
-#' @export
-#' 
-#' @examples
-#' #code to run
-#' 
-#' @return - the type of object that the function returns
-#'
-#' @author Andy Rominger <ajrominger@@gmail.com>
-#  @note other junk to mention
-#  @seealso - to provide pointers to other related topics
-#  @references - references to scientific literature on this topic
-#  @aliases - a list of additional topic names that will be mapped to this documentation when the user looks them up from the command line.
-#  @family - a family name. All functions that have the same family tag will be linked in the documentation.
-
 ##	function to compute log-likelihood of a meteDist object
 logLik.meteDist <- function(x) {
 	return(sum(x$fun@d(x$data,log=TRUE)))
 }
 
-logLikZ.meteDist <- function(x,nrep) {
+##  function to compute log-likelihood z-score
+logLikZ.meteDist <- function(x, nrep, return.sim=FALSE) {
 	lik.obs <- logLik(x)
 	lik.sim <- replicate(nrep, {
 		new.dat <- x$fun@r(length(x$data))
 		sum(x$fun@d(new.dat,log=TRUE))
 	})
 	
-	(lik.obs-mean(lik.sim))/sd(lik.sim)
+	if(return.sim) {
+		return(lik.sim)
+	} else {
+		return((lik.obs-mean(lik.sim))/sd(lik.sim))
+	}
 }
 
 
