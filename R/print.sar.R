32062a687b6ed9e708c420b2191f5191cdd3559a
ajrominger <ajrominger@gmail.com>
2015-10-09 13:59:25 -0600

    made help doc for print.sar

--- R/print.sar.R
+++ R/print.sar.R
@@ -1,3 +1,25 @@
+#' @title print.meteESF
+#'  
+#' @description \code{print.meteESF} prints an object of class \code{meteESF}
+#'
+#' @details
+#' See Examples
+#' 
+#' @param x an object of class \code{meteESF}
+#' @param ... arguments to be passed
+#' @keywords lagrange multiplier, METE, MaxEnt, ecosystem structure function
+#' @export
+#' 
+#' @examples
+#' data(anbo)
+#' anbo.sar <- meteSAR(anbo.new$spp, anbo.new$count, anbo.new$row, anbo.new$col, Amin=1, A0=16)
+#' print(anbo.sar)
+#' anbo.sar # alternatively
+#' 
+#' @return Returns the object silently
+#'
+#' @author Andy Rominger <ajrominger@@gmail.com>, Cory Merow
+
 print.sar <- function(x) {
 	cat(sprintf('%s %s area relationship ranging from \n', attr(x, 'source'), 
 	            ifelse(attr(x, 'type')=='sar', 'species', 'endemics')))
