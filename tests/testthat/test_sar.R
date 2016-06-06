8d9e8d8df209be6873a3dfb04e9f5d373c7289e7
ajrominger <ajrominger@gmail.com>
2016-06-06 16:04:38 -0700

    trying to work out the different equations in john's book to see which makes the serpentine figure

--- tests/testthat/test_sar.R
+++ tests/testthat/test_sar.R
@@ -21,8 +21,8 @@ test_that('EAR works for absolute and scaled area in the same way', {
 
 test_that('predicted EAR values are correct', {
   areas <- 2^(-1*(0:8)) * 256 # Serpentine areas
-  S0 <- 28
-  N0 <- 60346
+  S0 <- 24
+  N0 <- 37182
   A0 <- 256
   
   rawAxis <- c(48, 192, 334, 476, 614, 760, 900, 1040, 1182, 1322, 1466)
@@ -33,8 +33,27 @@ test_that('predicted EAR values are correct', {
   serpEAR <- downscaleSAR(meteESF(S0=S0, N0=N0), areas, A0=A0, EAR=TRUE)
   log(serpEAR$S) - actualMETE
   
+  this.esf <- meteESF(S0=S0, N0=N0, E0=10^10)
+  this.esf$La
+  
+  
+  
+  b <- 5.92e-05 #sum(this.esf$La)
+  n <- 1:N0
+  
+  eq7.74 <- sapply(areas, function(A) S0/log(1/b) * sum(exp(-b*n)/n * A0/(n*A + A0) * (n*A/(n*A + A0))^n))
+  log(eq7.74)
+  
+  earIdentity <- downscaleSAR(this.esf, A=A0, A0=A0)$S - downscaleSAR(this.esf, A0-areas[-1], A0=A0)$S
+  eq7.74[-1]
+  serpEAR$S[-1]
+  
+  par(mfrow=c(1, 4))
+  plot(actualMETE, log(eq7.74), xlim=c(-4, 2), ylim=c(-4, 2)); abline(0, 1)
+  plot(actualMETE, log(eq7.75), xlim=c(-4, 2), ylim=c(-4, 2)); abline(0, 1)
+  plot(actualMETE, log(eq7.76), xlim=c(-4, 2), ylim=c(-4, 2)); abline(0, 1)
+  plot(actualMETE, log(eq7.77), xlim=c(-4, 2), ylim=c(-4, 2)); abline(0, 1)
+  
   plot(areas, actualMETE, log='x')
   lines(areas, log(serpEAR$S))
 })
-
-
