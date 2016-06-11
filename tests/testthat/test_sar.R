9d204e9b3a0d90be99000b4395c9854ca2082c4d
ajrominger <ajrominger@gmail.com>
2016-06-10 18:26:07 -0700

    clarrified where state variables for tests come frome

--- tests/testthat/test_sar.R
+++ tests/testthat/test_sar.R
@@ -1,8 +1,7 @@
 context('SAR works')
-## values of S0, N0, A0 and predicted S for SAR from Newman et al. 
-
 
 test_that('predicted SAR values are correct', {
+  ## values of S0, N0, A0 and predicted S for SAR from Newman et al. 
   bell.sar <- meteSAR(S0=32, N0=920, Amin=1, A0=16)$pred$S
   newmanS <- c(12.2560715, 15.8991283, 20.2158684, 25.7856639, 32)
   expect_true(all(round(bell.sar, 3) == round(newmanS, 3)))
@@ -20,6 +19,7 @@ test_that('EAR works for absolute and scaled area in the same way', {
 
 
 test_that('predicted EAR values are correct', {
+  ## values of S0, N0, A0 and predicted S for Harte 2011
   areas <- 2^(-1*(0:8)) * 256 # Serpentine areas
   S0 <- 24
   N0 <- 37182
