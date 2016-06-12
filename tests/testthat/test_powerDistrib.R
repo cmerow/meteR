e973a2d217ef813daee5bccfe5975c31af59d6bf
ajrominger <ajrominger@gmail.com>
2016-06-11 17:17:37 -0700

    set-up file for testing power distribuitons

---
+++ tests/testthat/test_powerDistrib.R
@@ -0,0 +1,13 @@
+context('test of power distribution predictions')
+
+test_that('predicted rank ipd is correct', {
+  
+})
+
+test_that('predicted rank spd is correct', {
+  
+})
+
+test_that('predicted rank sipd is correct', {
+  
+})
